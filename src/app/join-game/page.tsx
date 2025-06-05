f052348af0a02d564aaad23c06e5117173cc1c19
pvsaint <vpeter817@gmail.com>
2025-06-05 17:56:41 +0100

    fix: merge conflict

--- src/app/join-game/page.tsx
+++ src/app/join-game/page.tsx
@@ -36,13 +36,6 @@ const AngledButton = ({
   variant = "arrow-right",
   size = "md",
 }: AngledButtonProps) => {
-
-  // const sizes = {
-  //   sm: "px-4 py-2 text-sm",
-  //   md: "px-6 py-3 text-base",
-  //   lg: "px-8 py-4 text-lg",
-  // } as const;
-
   const buttonClasses = `
     relative inline-block
     font-medium
