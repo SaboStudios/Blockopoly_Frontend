06e03790cf4b9c0ab31b2bac9f0649251de0b984
Victor Peter <vpeter817@gmail.com>
2025-06-06 14:56:43 +0100

    feat: implement join game screens (#9)
    * feat: implement join game screens
    
    * fix: merge conflict
    
    * fix: remove package-lock.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/navbar";
+// import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <div className="">
-<Navbar/>
+{/* <Navbar/> */}
     </div>
   );
 }
