77e46766cd76ac866c19134509a906ee6998e440
Emmanuel <eolasunkanmi@gmail.com>
2025-06-07 02:04:49 +0100

    updating the blockoploy landing page

--- src/app/components/Landing-page/home.tsx
+++ src/app/components/Landing-page/home.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { InputField } from "@/app/components/input-field";
+import ButtonBg from "@/app/components/button"
 
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <section className="relative">
       <div>
-          <Image src="/bg gradient.png" alt="bg-color" width={1440} height={1024} className="z-1 absolute top-0 border-2 border-red-600"/>
+          <Image src="/bg gradient.png" alt="bg-color" width={1440} height={1024} className="z-1 absolute top-0"/>
            <Image src="/board.png" alt="bg-color" width={1440} height={1024} className="z-0" />
            {/* <div className="max-w-[766px] h-[451px] mx-auto gap-[24px] border-2 border-yellow-400">
         container
@@ -28,12 +29,12 @@ export default function Home() {
 
             <div className="relative">
               {/* <Image src="/the Block.png" alt="block-image" width={766} height={120}/> */}
-              <h1 className="w-[47.875rem] font-[900] text-[7.25rem] font-[Orbitron] leading-[7.5rem] tracking-tight capitalize m-0">THE BLOCK</h1>
+              <h1 className="w-[47.875rem] font-[900] text-[7.25rem] font-[Orbitron] leading-[7.5rem] tracking-tight capitalize m-0 bloc-color text-shadow">THE BLOCK</h1>
               <Link href="#">
                 <Image src="/QuestionMark.png" 
                   alt="block-image" width={27} height={27} 
                   className="absolute top-[112.02px] left-[522.51px] pt-[0.83px]
-                  pr-[5.8px] pb-[0.83px] pl-[5.8px] gap-[2.76px] rotate-[12 deg]"
+                  pr-[5.8px] pb-[0.83px] pl-[5.8px] gap-[2.76px] rotate-[12deg]"
                 />
               </Link>
 
@@ -46,9 +47,20 @@ export default function Home() {
             </div>
         </div>
 
-          <div className="w-[21.6875rem] h-[11.9375rem] mt-[2rem] border-2 border-red-500 mx-auto">
-           <InputField />
-           
+          <div className="w-[21.6875rem] h-[11.9375rem] mt-[2rem] mx-auto gap-[2rem] flex flex-col">
+            <div className="w-[16.25rem] h-[7.4375rem] gap-[1rem] flex flex-col mx-auto">
+              <InputField />
+                <ButtonBg />
+            </div>
+          
+          <div className="w-[21.6875rem] h-[2.5rem] flex gap-4">
+            <button className="w-[8.75rem] h-[2.5rem]">
+              <h3 className="w-[5.8125rem] h-6 top-[8px] left-[15px] gap-[4px] flex font-[DM sans] text-[#0FF0FC]"><span></span>Join Room</h3>
+            </button>
+              <button className="w-[14.1875rem] h-[2.5rem] rounded-[0.375rem] border-2 border-blue-600 bg-[#003B3E]">
+                  <h3 className="w-[10.875rem] h-6 left-[9.6875rem] gap-[4px] top-[10px] font-[DM sans] rounded-[6px] text-[#0FF0FC]"><span></span>Create A Private Game</h3>
+              </button>
+          </div>
 
           </div>
 
---
+++ src/app/components/button.tsx
@@ -0,0 +1,17 @@
+"use Client"
+
+// import Image from "next/image";
+
+export default function ButtonBg() {
+  return (
+    <div className="relative w-[16.25rem] h-[7.4375rem]mx-auto">
+      <div className="butt-bg text-center">
+          <button className="mt-4.5">
+            <h3 className="w-[8.0625rem] h-[1.875rem] font-[Orbitron] font-[700] text-2xl leading-[100%] tracking-[-10%] text-[#010F10] p-0">Let’s Go!</h3>
+          </button>
+      </div>
+        {/* <div className="triangle"></div> */}
+         
+    </div>
+  )
+}
