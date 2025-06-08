edcbaea50ffa0cecffeaac77ebb127eb3bd9103a
Iwueseiter <preciouschia19@gmail.com>
2025-06-08 23:25:10 +0100

    Feat: implement player appearance selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 // import Navbar from "./components/navbar";
+import ColorSelection from "./playerselection/selection";
 
 export default function Home() {
   return (
     <div className="">
 {/* <Navbar/> */}
+<ColorSelection />
     </div>
   );
 }
