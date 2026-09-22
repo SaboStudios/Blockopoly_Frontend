9f4660e2049fd4ed782947d317ab777be995ba0e
Ajidokwu Sabo <aji70@users.noreply.github.com>
2026-09-22 14:35:27 +0000

    fix: #147 [Handsoff test 052] Add aria-label to icon button #52
    Closes #147

--- components/game/chat-room.tsx
+++ components/game/chat-room.tsx
@@ -8,7 +8,7 @@ const ChatRoom = () => {
             {/* top */}
             <div className="w-full h-[37px] flex justify-between items-center border-b-[1px] border-[#263238] px-4">
                 <h4 className="font-[700] font-dmSans text-[#F0F7F7] text-[14px]">Chat</h4>
-                <Users className='w-4 h-4 text-[#F0F7F7]' />
+                <Users className='w-4 h-4 text-[#F0F7F7]' aria-hidden="true" />
             </div>
             {/* content */}
             <main className="w-full h-[calc(100%-89px)] overflow-y-auto no-scrollbar flex justify-center items-center">
@@ -17,11 +17,11 @@ const ChatRoom = () => {
 
             {/* bottom */}
             <div className="w-full border-t-[1px] border-[#263238] h-[52px] flex items-stretch gap-2 p-2">
-                <input type="text" className="outline-none flex-1 bg-[#0B191A] rounded-[20px] text-[12px] text-[#AFBAC0] font-dmSans px-3" name="chat" id="chat" placeholder='Type a message...' />
+                <input type="text" className="outline-none flex-1 bg-[#0B191A] rounded-[20px] text-[12px] text-[#AFBAC0] font-dmSans px-3" name="chat" id="chat" placeholder='Type a message...' aria-label="Chat message" />
 
                 {/* send btn */}
-                <button className='size-[36px] rounded-[20px] bg-[#010F10] border-[1px] border-[#263238] flex items-center justify-center text-[#AFBAC0]'>
-                    <Send className="w-5 h-5" />
+                <button type="button" aria-label="Send message" className='size-[36px] rounded-[20px] bg-[#010F10] border-[1px] border-[#263238] flex items-center justify-center text-[#AFBAC0]'>
+                    <Send className="w-5 h-5" aria-hidden="true" />
                 </button>
             </div>
         </div>
